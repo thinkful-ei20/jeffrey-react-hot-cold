057fa65e31427d738080280c6f8d76985f308d8f
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-17 11:03:40 -0500

    Create basic state

--- src/components/game.js
+++ src/components/game.js
@@ -8,13 +8,21 @@ import GuessList from './guess-list';
 export default class Game extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            guesses: [],
+            answer: 10,
+            feedback: 'Make your guess!!',
+        }
+    }
+
+    setGuesses(guess) {
     }
 
     render() {
         return (
             <div>
                 <Header />
-                <GuessSection feedback="Make your guess!" />
+                <GuessSection feedback={this.state.feedback} />
                 <GuessCount count={3} />
                 <GuessList guesses={[10, 15, 25]} />
             </div>
