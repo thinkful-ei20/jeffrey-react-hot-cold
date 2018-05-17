aa927dc6b3fa9e0e4db92ae85fe93a776a84bd2d
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-05-17 11:17:05 -0500

    Using state data

--- src/components/game.js
+++ src/components/game.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import Header from './header';
 import GuessSection from './guess-section';
-import GuessCount  from './guess-count';
+import GuessCount from './guess-count';
 import GuessList from './guess-list';
 
 export default class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            guesses: [],
-            answer: 10,
+            guesses: [10, 15, 25, 50],
+            answer: 99,
             feedback: 'Make your guess!!',
         }
     }
@@ -23,8 +23,8 @@ export default class Game extends React.Component {
             <div>
                 <Header />
                 <GuessSection feedback={this.state.feedback} />
-                <GuessCount count={3} />
-                <GuessList guesses={[10, 15, 25]} />
+                <GuessCount count={this.state.guesses.length} />
+                <GuessList guesses={this.state.guesses} />
             </div>
         );
     }
